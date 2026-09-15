7367d4ac9fbb3639c5c957b12036a7b2045bf0bc
IberianFootballLeague <iberianfootballleague.oficial@gmail.com>
2026-09-15 21:42:58 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
 const SUPABASE_URL = "https://boazhychmpxeuplyxzsi.supabase.co";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     const discordButton = document.getElementById("discord-login");
 
-    discordButton.addEventListener("click", async () => {
-        const redirectTo = window.location.origin + window.location.pathname;
+    // Comprobar si existe una sesión de Supabase
+    const response = await fetch(
+        SUPABASE_URL + "/auth/v1/user",
+        {
+            headers: {
+                "apikey": SUPABASE_ANON_KEY,
+                "Authorization": "Bearer " + accessToken
+            }
+        }
+    );
+
+    if (response.ok) {
+        const user = await response.json();
+        console.log("Usuario conectado:", user);
+    }
+
+    discordButton.addEventListener("click", () => {
+        const redirectTo =
+            window.location.origin + window.location.pathname;
 
         const loginUrl =
             SUPABASE_URL +
